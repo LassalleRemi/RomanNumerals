19a1957fe728f4e96f764d280fe7eda9cc012338
Lassalle Remi <remil.lassalle@gmail.com>
2016-04-22 14:24:17 +0200

    4 test valides

--- RomanNumerals/src/main/java/iut/tdd/RomanNumerals.java
+++ RomanNumerals/src/main/java/iut/tdd/RomanNumerals.java
@@ -1,12 +1,19 @@
 package iut.tdd;
 
 public class RomanNumerals {
+	String[] r = new String[]{"I","V","X","L","D","C","M"};
+	int[] n = new int[]{1,5,10,50,100,500,1000};
+	
 	public String convertToRoman(String arabe) {
-		return "I";
+		if (arabe.equals("1")) return "I";
+		else if (arabe.equals("5")) return "V";
+		else return "";
 	}
 
 	public String convertFromRoman(String roman) {
-		return "1";
+		if (roman.equals("I")) return "1";
+		else if (roman.equals("V")) return "5";
+		else return "";
 	}
 
 }
